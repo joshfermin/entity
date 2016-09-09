a98d0420e5766c8e9cb775cca29eabf220359bfa
joshfermin <joshfermin@gmail.com>
2016-09-09 08:43:54 -0600

    trying to get audio-streaming to work...

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
--- server/routes/sockets.js
+++ server/routes/sockets.js
@@ -1,16 +1,33 @@
 // sockets.js
-
 exports.socketServer = function (server) {
     var io = require('socket.io')(server);
+    var ss = require('socket.io-stream');
+    var fs = require('fs');
+
+    var lame = require('lame');
+    var Speaker = require('speaker');
 
     io.on('connection', function(socket){
+
+        ss(socket).on('file', function(stream){
+            var filePath = '/Users/joshfermin/IdeaProjects/sound/server/mp3/cliffs.mp3';
+            var readStream = fs.createReadStream(filePath);
+
+            readStream.pipe(new lame.Decoder)
+                .on('format', console.log)
+                .pipe(new Speaker)
+                .pipe(stream);
+        });
+
+
         socket.on('chat message', function(msg){
             io.emit('chat message', msg);
         });
         socket.on('disconnect', function(){
-            console.log('user disconnected');
+            io.emit('chat message', 'User Disconnected');
         });
     });
+
     var chat = io
         .of('/chat')
         .on('connection', function (socket) {
